806883e26b1c2eb8b4f6f5a00cb10328b3bf0537
Methminpulsara <methminpulsara10@gmail.com>
2025-04-29 12:35:54 +0530

    create addEmployee Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { DhashboardComponent } from './page/dhashboard/dhashboard.component';
+import { AddEmployeeComponent } from './page/dhashboard/add-employee/add-employee.component';
 
 export const routes: Routes = [
     {
-        path: "",
-        component:DhashboardComponent
+        path: "dashboard",
+        component:DhashboardComponent,
+        children:[
+          {
+            path:"add",
+            component:AddEmployeeComponent
+          }
+        ]
     }
 
 ];
