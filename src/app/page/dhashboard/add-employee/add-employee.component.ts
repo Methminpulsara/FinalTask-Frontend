d2bac2a0df8975c7623464ff705455a8fa2bd7d5
Methminpulsara <methminpulsara10@gmail.com>
2025-04-29 13:38:22 +0530

    create add api

--- src/app/page/dhashboard/add-employee/add-employee.component.ts
+++ src/app/page/dhashboard/add-employee/add-employee.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import EmployeeService from '../../../../service/EmployeeService';
+import Employee from '../../../../model/Employee';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-employee',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
 export class AddEmployeeComponent {
 
-}
+
+  constructor(private employeeService:EmployeeService){}
+
+  public employee : Employee= {
+    employeeId: 0,
+    name: '',
+    email: '',
+    department: '',
+    createdDate: '',
+    modifiedDate: ''
+  }
+
+  add(){
+    this.employeeService.save(this.employee).subscribe(res=>{
+      this.resetform();
+      alert("added succccsfully ! ")
+    })
+
+  }
+
+
+  resetform(){
+      this.employee ={
+      employeeId: 0,
+      name: '',
+      email: '',
+      department: '',
+      createdDate: '',
+      modifiedDate:""
+    }
+  }
+  }
